70770a1b3087d32c6708c283cfb9a4f477c75525
Joy Ahmed <dev.joyahmed@gmail.com>
2023-08-14 07:49:41 +0600

    added heros reducer & action

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 import rootResucer from "./reducer/index.ts";
+import { addCharacterById } from "./actions/index.ts";
 
 const store = createStore(rootResucer);
 
 console.log("store.getState: ", store.getState());
+store.subscribe(() => console.log("store", store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import characters_json from '../data/character.json';
 import { ADD_CHARACTER } from '../actions';
 
@@ -14,6 +15,10 @@ type CharacterAction = {
     type: string,
     id: number
 }
+type HerosAction = {
+    type: string,
+    id: number
+}
 
 function characters(state: Character[] = characters_json, action: CharacterAction){
     switch(action.type){
@@ -24,5 +29,23 @@ function characters(state: Character[] = characters_json, action: CharacterActio
     }
 }
 
+function heros(state = [], action: HerosAction) {
+    switch(action.type) {
+        case ADD_CHARACTER:
+            return [...state, createCharacter(action.id)]
+        default:
+            return state
+    }
+}
 
-export default characters;
+
+function createCharacter(id: number) {
+    return characters_json.find(c => c.id === id);
+}
+
+const rootReducer = combineReducers({
+    characters,
+    heros
+})
+
+export default rootReducer;
